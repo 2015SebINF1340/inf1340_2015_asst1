76fd99064a692a0350d284e654c00a991d59d568
2015SebINF1340 <seb_dagenais@yahoo.ca>
2015-10-07 04:00:35 -0400

    Update exercise1.py
    having trouble committing and pushing so I'm putting this here for the moment...

--- exercise1.py
+++ exercise1.py
@@ -13,5 +13,19 @@ __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
 
-money = 1000.00
-print(money)
+#just a preliminary attempt at wrapping my head around this..
+#I don't think Im very good at this...   ...yet...
+sharePrice = raw_input('How much did Lakshmi pay per share?')
+brokerCost = raw_input('What percentage did she pay her broker?')
+print ('commission per share:')
+print (float(sharePrice)*0.03)
+shareQuantity = raw_input('How many shares did Lakshmi buy?')
+totalCost = (float(sharePrice)*float(shareQuantity))
+commissionPaid = (float(sharePrice)*float(shareQuantity))*0.03
+moneyLeft = (float(totalCost-commissionPaid))
+print ('total cost =')+ str(totalCost)
+#print (float(sharePrice)*float(shareQuantity))
+print ('This means Lakshmi paid her broker a total of:')
+print commissionPaid
+print ('This leaves Lakshmi with:')
+print moneyLeft
