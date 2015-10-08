1a5581b0e7ef265a38a12bab2bfef4cc072c4c30
owusuthe <therese.owusu@mail.utoronto.ca>
2015-10-08 11:28:56 -0400

    Updated exercise 1 and included commnets

--- exercise1.py
+++ exercise1.py
@@ -25,8 +25,6 @@ __author__ = 'Susan Sim'
 __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
-money = 1000.00
-print(money)
 
 #Input the number of shares bought
 #Input the cost of each share
@@ -40,13 +38,12 @@ cost_share = (number_share * price_share)
 broker_commission = .03 * (number_share * price_share)
 bought_total = cost_share + broker_commission
 
-#print(cost_share)
-#print(broker_commission)
-#print(bought_total)
 
 #Input the number of shares sold
 #Input the price of each share sold
+#Calculate the total stock sold
 #Calculate the stockbroker's commission
+#Calculate the total sold
 
 number_share_sold = 2000.0
 price_share_sold = 942.75
@@ -54,11 +51,8 @@ cost_share_sold = (number_share_sold * price_share_sold)
 broker_commission_sold = .03 * (number_share_sold * price_share_sold)
 sold_total = cost_share_sold + broker_commission_sold
 
-#print(cost_share_sold)
-#print(broker_commission_sold)
-#print(sold_total)
 
-#Calculate amount Lakshmi had left after selling and paying the broker
+#Calculate amount Lakshmi had left after selling the stock and paying the broker
 sold_stock = cost_share_sold - cost_share
 remainder = sold_stock - (broker_commission_sold + broker_commission)
 print(remainder)
